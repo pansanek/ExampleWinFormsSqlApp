6426ad93658e768f9cf91271c7f93e65c24a5e09
SashaPotemkin <alex.potjomkin@gmail.com>
2024-09-05 12:14:13 +0300

    Подключение к бд

---
+++ ExampleWinFormsSqlApp/DB.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWinFormsSqlApp
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3307;username=root;password=root;database=winformsapp");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
